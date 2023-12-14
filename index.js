a13c3481c97d35dc14bd1340da7cc1cb5491469c
Amit Sing <singh.amitabes@gmail.com>
2023-12-14 16:09:56 +0530

    firs20t commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var cors = require('cors')
 
 const dbConnect = require("./DbConnect/dbConnect")
 
-const PORT =process.env.PORT || 4000
+const PORT = process.env.PORT || 4000
 dbConnect()
 const app = express()
 
@@ -13,14 +13,14 @@ app.use('/public', express.static('public'));
 
 app.use(cors())
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))                  
+app.use(express.urlencoded({ extended: true }))
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
-app.use("/ems", require('./Route/route') )
+app.use("/ems", require('./Route/route'))
 
-app.listen(PORT,()=>console.log(`server is start on ${PORT}`))
+app.listen(PORT, () => console.log(`server is start on ${PORT}`))
